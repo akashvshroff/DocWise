f6bd4096724831d829f14593e1bfccdc4aee7b76
Akash Shroff <63399889+akashvshroff@users.noreply.github.com>
2023-06-13 12:43:52 -0500

    util functions

--- utils.py
+++ utils.py
@@ -6,3 +6,10 @@ def does_file_have_pdf_extension(file):
         st.warning('Not a valid PDF file.', icon="⚠️")
         return False
     return True
+
+def store_pdf_file(file, dir):
+    file_path = os.path.join(dir, file.name)
+    with open(file_path, 'wb') as fhand:
+        fhand.write(file.getbuffer())
+    return file_path
+
