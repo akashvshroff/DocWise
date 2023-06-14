24429f60480e09ed83fc301850e90e1a47ef11e8
Akash Shroff <63399889+akashvshroff@users.noreply.github.com>
2023-06-14 16:23:24 -0500

    updated todo

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-#TODO:
-#1. Streamlit - doc input, validation and local storage + styling with lottie
-#2. LangChain - doc upload, API calls, similarity search
-
 #general imports
 import os
 import requests
